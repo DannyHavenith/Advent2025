a003dd2d84ea4259ef93cbb37bc44269d695c325
Danny <dh.git@havenith.net>
2025-12-06 19:16:16 +0100

    Solve day6

---
+++ day6/day6a.cpp
@@ -0,0 +1,77 @@
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <regex>
+#include <string>
+
+using Number = std::int64_t;
+
+struct Accumulator {
+    Accumulator( Number initialProduct, Number initialSum)
+    :product{initialProduct}, sum{initialSum}
+    {}
+
+    Number product;
+    Number sum;
+};
+
+using Accumulators = std::vector<Accumulator>;
+using Tokenizer = std::sregex_token_iterator;
+
+int main()
+{
+    std::ifstream input{"input6.txt"};
+    std::string line;
+    Accumulators accumulators;
+
+    const std::regex matchNumber{"(\\d+)"};
+
+    // Read the first line and initialize an Accumulator vector of the right size.
+    getline( input, line);
+    for (
+        auto i = Tokenizer{ line.begin(), line.end(), matchNumber, 1};
+        i != Tokenizer{};
+        ++i)
+    {
+        auto number = stoi( *i);
+        accumulators.emplace_back( number, number);
+    }
+
+    // Scan the lines and keep a running sum and product for the columns.
+    while (getline(input, line))
+    {
+        bool parsedANumber = false;
+        auto currentAccumulator = accumulators.begin();
+
+        for ( auto i = Tokenizer{ line.begin(), line.end(), matchNumber, 1} ;
+              i != Tokenizer{};
+              ++i, ++currentAccumulator)
+        {
+            assert( currentAccumulator != accumulators.end());
+
+            parsedANumber = true;
+            auto number = stoi( *i);
+            *currentAccumulator = {currentAccumulator->product * number, currentAccumulator->sum + number};
+        }
+        if (not parsedANumber) break; // no numbers, so this is the line with operators.
+    }
+
+    // For each column now either take the sum or the product and add it to the
+    // total.
+    const std::regex matchOperator{"([\\+\\*])"};
+    auto currentAccumulator = accumulators.begin();
+    Number sum = 0;
+    for (
+        auto currentOp = Tokenizer{line.begin(), line.end(), matchOperator, 1};
+        currentOp != Tokenizer{};
+        ++currentOp, ++currentAccumulator)
+    {
+        sum += *currentOp == '+' ?
+                    currentAccumulator->sum :
+                    currentAccumulator->product;
+    }
+
+    std::cout << "sum = " << sum << '\n';
+
+    return 0;
+}
---
+++ day6/day6b.cpp
@@ -0,0 +1,77 @@
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <optional>
+#include <regex>
+#include <string>
+
+using Number = std::int64_t;
+using Accumulator = std::optional<Number>;
+using Accumulators = std::vector<Accumulator>;
+
+int main()
+{
+    std::ifstream input{"input6.txt"};
+    Accumulators accumulators;
+
+    std::string line;
+    getline( input, line);
+    std::transform(
+        line.begin(), line.end(),
+        std::back_inserter( accumulators),
+        []( char maybeDigit){ return maybeDigit == ' ' ? std::nullopt : Accumulator{ maybeDigit - '0'};}
+    );
+
+    const std::regex numbersLine{"([\\d\\s])+"};
+    while (getline(input, line) and regex_match( line, numbersLine))
+    {
+        if (line.size() > accumulators.size()) accumulators.resize( line.size());
+
+        assert( line.size() == accumulators.size());
+        auto currentCharacter = line.begin();
+        for ( auto &ac : accumulators)
+        {
+            if (*currentCharacter != ' ')
+            {
+                const auto value = *currentCharacter - '0';
+                ac = ac ? *ac * 10 + value : value;
+            }
+            ++currentCharacter;
+        }
+    }
+
+    Number sum = 0;
+
+
+    static const std::regex op{R"(([\+\*]))"};
+    auto currentAccumulator = accumulators.begin();
+    using Tok = std::sregex_token_iterator;
+    for ( auto currentOp = Tok{line.begin(), line.end(), op, 1}; currentOp != Tok{}; ++currentOp, ++currentAccumulator)
+    {
+        while (not currentAccumulator->has_value()) ++currentAccumulator;
+        assert( currentAccumulator != accumulators.end());
+
+        if (*currentOp == '+')
+        {
+            Number subSum = 0;
+            while( currentAccumulator != accumulators.end() and currentAccumulator->has_value())
+            {
+                subSum += **currentAccumulator++;
+            }
+            sum += subSum;
+        }
+        else
+        {
+            Number subSum = 1;
+            while( currentAccumulator != accumulators.end() and currentAccumulator->has_value())
+            {
+                subSum *= **currentAccumulator++;
+            }
+            sum += subSum;
+        }
+    }
+
+    std::cout << "sum = " << sum << '\n';
+
+    return 0;
+}
